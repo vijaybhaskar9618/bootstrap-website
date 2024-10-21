5e7df4556c8a527435faedad594119456050cba4
vijaybhaskar9618 <mandedhavijaybhaskar@gmail.com>
2024-10-21 18:54:15 +0530

    commit

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.logs').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
+$(document).ready(function () {
+    $('.testslide').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1990,
+    });
+});
+
